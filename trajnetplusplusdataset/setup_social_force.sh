9f90ffe791a65c2786a7135f821c034aed79d7d6
Ranveer Singh <sidana.manik251@gmail.com>
2023-04-09 12:52:29 +0530

    Delete trajnetplusplusdataset directory

--- trajnetplusplusdataset/setup_social_force.sh
+++
@@ -1,9 +0,0 @@
-## Additional Requirements (Social Force)
-wget https://github.com/svenkreiss/socialforce/archive/refs/heads/main.zip
-unzip main.zip
-rm main.zip
-
-## Setting up Social Force
-cd socialforce-main/
-pip install -e .
-cd ../
